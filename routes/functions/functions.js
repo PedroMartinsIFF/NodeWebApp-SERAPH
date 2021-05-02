172eb9972232400812392e5b9214106795f91f21
PedroMartinsIFF <pedroatpiff@gmail.com>
2021-05-02 17:41:46 -0300

    update com dados mais atual

--- routes/functions/functions.js
+++ routes/functions/functions.js
@@ -1,18 +1,19 @@
 const spawn = require('child_process').spawn;
 const process = spawn ('python3', ['../script1.py']);
 
+
 function kops_create(value) {
     const { exec } = require("child_process");
         exec("kops create cluster \
-        --state=${KOPS_STATE_STORE} \
+        --state="+ value.field3+" \
         --node-count="+ value.field1+" \
-        --master-size=t2.medium \
-        --node-size=t2.medium \
+        --master-size="+ value.field4+" \
+        --node-size="+ value.field4+" \
         --zones=us-east-1a \
-        --name=${KOPS_CLUSTER_NAME} \
+        --name="+ value.field5+" \
         --ssh-public-key=/home/ec2-user/.ssh/id_rsa.pub \
         --dns private \
-        --master-count="+value.field2+" \
+         --master-count="+value.field2+" \
         --yes", (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`);
@@ -25,7 +26,6 @@ function kops_create(value) {
         }
         if (stdout) {
             console.log(`stdout: ${stdout}`);
-            return 0;
         }
                 
     });
@@ -50,23 +50,29 @@ function verify_2() {
 
 function verify() {
     const { exec } = require("child_process");
+        retorno = 0;
         exec("kops validate cluster", (error, stdout, stderr) => {
         if (error) {
+            retorno = 0;
             console.log("Aguardando Validação");
-            return 0;   
+            
+ 
         } 
         if (stdout) {
             console.log(`stdout: ${stdout}`);
             
-            return 1;
+            
+            retorno = 1;
         }
+
+        return retorno;
     });
 
 }
 
 function dask_install(){
     const { exec } = require("child_process");
-    exec("helm install dask dask/dask", (error, stdout, stderr) => {
+    exec("helm install dask dask/dask -f values.yaml", (error, stdout, stderr) => {
     if (error) {
         console.log("Aguardando Validação");
         return 0;   
